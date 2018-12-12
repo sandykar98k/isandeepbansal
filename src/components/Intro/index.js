74d93f611460fb36eb2832761e8e67d19ec382b7
sandykar98k <iambansalsandeep@gmail.com>
2018-12-12 10:55:31 +0530

    Cards with image with media query

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -1,31 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import devImg from '../../assets/devImage.svg';
 
-class Intro extends Component {
-  render() {
-    return (
-      <div class="jumbotron jumbotron-fluid intro-jumbotron">
-        <div class="container">
-          <div className="row intro-section">
-            <div className="col-sm-12 col-md-6 col-lg-6">
-              <h1 class="intro-heading">
-                A freelance front end developer for digital products
-              </h1>
-              <p className="intro-description">
-                I have a diverse set of skills, ranging from, HTML + CSS +
-                JAVASCRIPT + REACT.JS all the way to node js. I can create
-                interactive UIs, using component based architecture which is
-                more scalable and reusable.
-              </p>
-            </div>
-            <div className="col-sm-12 col-md-6 col-lg-6">
-              <img className="intro-img img-fluid" src={devImg} alt="dev" />
-            </div>
+const Intro = () => {
+  return (
+    <div className="jumbotron jumbotron-fluid intro-jumbotron">
+      <div className="container">
+        <div className="row intro-section">
+          <div className="col-sm-12 col-md-6 col-lg-6">
+            <h1 className="intro-heading">
+              A freelance front end developer for digital products
+            </h1>
+            <p className="intro-description">
+              I have a diverse set of skills, ranging from, HTML + CSS +
+              JAVASCRIPT + REACT.JS all the way to node js. I can create
+              interactive UIs, using component based architecture which is more
+              scalable and reusable.
+            </p>
+          </div>
+          <div className="col-sm-12 col-md-6 col-lg-6">
+            <img className="intro-img img-fluid" src={devImg} alt="dev" />
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Intro;
--- src/components/MySkills/index.js
+++ src/components/MySkills/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-
+import htmlImg from '../../assets/html.svg';
+import javascriptImg from '../../assets/javascript.svg';
+import reactImg from '../../assets/reactjs.png';
 const MySkills = () => {
   return (
     <section className="my-skills-sec">
@@ -12,32 +14,54 @@ const MySkills = () => {
         <div className="row my-skills-body">
           <div className="col-sm-12 col-md-4 col-lg-4">
             <div className="card card-body my-skills-card">
-              <h4>HTML, CSS</h4>
-              <span>1 Years, 10 Projects</span>
-              <br />
-              <span>
+              <div className="my-skills-card-header">
+                <div className="my-skills-card-header-left">
+                  <img src={htmlImg} className="img-fluid" alt="html" />
+                </div>
+                <div className="my-skills-card-header-right">
+                  <h4>HTML, CSS</h4>
+                  <span>1 Years, 10 Projects</span>
+                </div>
+              </div>
+              <div className="my-skills-card-body">
                 If i have to rate myslef in HTML, CSS i will rate 9 out of 10
-              </span>
+              </div>
             </div>
           </div>
           <div className="col-sm-12 col-md-4 col-lg-4">
             <div className="card card-body my-skills-card">
-              <h4>JavaScript</h4>
-              <span>1 Years, 10 Projects</span>
-              <br />
-              <span>
-                If i have to rate myslef in JavaScript i will rate out 8 of 10
-              </span>
-            </div>
-          </div>
-          <div className="col-sm-12 col-md-4 col-lg-4">
-            <div className="card card-body my-skills-card">
-              <h4>ReactJS</h4>
-              <span>1 Years, 4 Projects</span>
-              <br />
-              <span>
+              <div className="my-skills-card-header">
+                <div className="my-skills-card-header-left">
+                  <img src={reactImg} className="img-fluid" alt="react" />
+                </div>
+                <div className="my-skills-card-header-right">
+                  <h4>ReactJS</h4>
+                  <span>1 Years, 4 Projects</span>
+                </div>
+              </div>
+              <div className="my-skills-card-body">
                 If i have to rate myslef in ReactJS i will rate 8 out of 10
-              </span>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-12 col-md-4 col-lg-4">
+            <div className="card card-body my-skills-card">
+              <div className="my-skills-card-header">
+                <div className="my-skills-card-header-left">
+                  <img
+                    src={javascriptImg}
+                    className="img-fluid"
+                    alt="javascript"
+                  />
+                </div>
+                <div className="my-skills-card-header-right">
+                  <h4>javascript</h4>
+                  <span>1 Years, 8 Projects</span>
+                </div>
+              </div>
+              <div className="my-skills-card-body">
+                If i have to rate myslef in JavaScript i will rate 8 out of 10
+              </div>
             </div>
           </div>
         </div>
