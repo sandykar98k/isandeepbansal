c9d88c65b808057bc43b354ed20123e169bfe858
sandykar98k <iambansalsandeep@gmail.com>
2018-12-15 20:29:16 +0530

    Footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div id="footer" className="d-none d-lg-block">
+      <div className="Footer-Content">© isandeepbansal.com</div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,16 +3,18 @@ import React from 'react';
 import Header from '../Header';
 import HomePage from '../Pages/HomePage';
 import MobileNav from '../MobileNav';
+import Footer from '../Footer';
 
 const Layout = () => {
   return (
-    <React.Fragment>
+    <div id="container">
       <Header />
-      <div className="container-fluid p-0">
+      <div id="body" className="container-fluid pr-0 pl-0">
         <HomePage />
       </div>
+      <Footer />
       <MobileNav />
-    </React.Fragment>
+    </div>
   );
 };
 
